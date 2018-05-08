c2a9da3d463ad7195671df62b3b3592bf3764213
flan <flan@flande.re>
2018-05-08 10:53:17 +0200

    Initial commit

---
+++ src/vndb.erl
@@ -0,0 +1,86 @@
+-module(vndb).
+-export([connect/0, connect/3, cmd/2, close/1]).
+-export([login/2, dbstats/1, get/4, get/5]).
+-export([nyaa/1]).
+
+% TODO: timeouts? send/2 and recv/2 have them set to infinity
+
+% --- basic interface
+
+connect() -> connect("api.vndb.org", 19535, true).
+connect(Host, Port, SSL) ->
+	Opts = [binary, {packet, 0}, {active, false}],
+	case SSL of
+		false ->
+			{ok, Sock} = gen_tcp:connect(Host, Port, Opts),
+			{vndb, Sock, fun gen_tcp:send/2, fun gen_tcp:recv/2, fun gen_tcp:close/1};
+		true ->
+			{ok, Sock} = ssl:connect(Host, Port, Opts),
+			{vndb, Sock, fun ssl:send/2, fun ssl:recv/2, fun ssl:close/1}
+	end.
+
+cmd({vndb, S, Send, Recv, _}, Cmd) ->
+	ok = Send(S, [Cmd, <<4>>]),
+	{ok, R} = Recv(S, 0),
+
+	% according to https://vndb.org/d11#2 there's no strictly defined format for
+	% responses, but right now they all follow a single convention: name,
+	% optionally followed by a single space and JSON content, terminated with \x04
+	case cmd_restype(R) of
+		%{nospace, noterm} -> TODO
+		{nospace, Term} ->
+			<<Name:Term/binary, 4>> = R,
+			binary_to_atom(Name, utf8);
+		{Space, noterm} ->
+			<<Name:Space/binary, " ", Rest>> = R,
+			{incomplete, F} = jsx:decode(Rest, [stream]),
+			cmd_recvmore(S, Recv, F, binary_to_atom(Name, utf8));
+		{Space, Term} ->
+			Restlen = Term - Space - 1,
+			<<Name:Space/binary, " ", Rest:Restlen/binary, 4>> = R,
+			{binary_to_atom(Name, utf8), jsx:decode(Rest)}
+	end.
+
+cmd_restype(R) ->
+	Match = binary:match(R, <<" ">>),
+	Space = case Match of
+		nomatch -> nospace;
+		{Start, _} -> Start
+	end,
+	{Space, cmd_term(R)}.
+
+cmd_term(R) ->
+	Rlen = byte_size(R) - 1,
+	case R of
+		<<_:Rlen/binary, 4>> -> Rlen;
+		_Else -> noterm
+	end.
+
+cmd_recvmore(S, Recv, F, Cmdname) ->
+	{ok, Response} = Recv(S, 0),
+	case cmd_term(Response) of
+		noterm ->
+			{incomplete, G} = F(Response),
+			cmd_recvmore(S, Recv, G, Cmdname);
+		Term ->
+			<<Rest:Term/binary, 4>> = Response,
+			{incomplete, G} = F(Rest),
+			{Cmdname, G(end_stream)}
+	end.
+
+close({vndb, S, _, _, Close}) -> ok = Close(S).
+
+% --- wrapped commands
+
+login(V, Args) -> vndb:cmd(V, [<<"login ">>, jsx:encode(Args)]).
+dbstats(V) -> vndb:cmd(V, <<"dbstats">>).
+get(V, Type, Flags, Filters) -> get(V, Type, Flags, Filters, #{}).
+get(V, Type, Flags, Filters, Options) ->
+	{results, R} = vndb:cmd(V, [
+		<<"get ">>,
+		atom_to_binary(Type, utf8), <<" ">>,
+		lists:join(<<",">>, lists:map(fun(X) -> atom_to_binary(X, utf8) end, Flags)), <<" ">>,
+		Filters, <<" ">>,
+		jsx:encode(Options)
+	]),
+	R.
