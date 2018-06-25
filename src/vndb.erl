2cd9e747761b9cfc76c43a12001cb2c8e3c83600
flan <flan@flande.re>
2018-06-25 16:11:19 +0200

    Remove undefined function

--- src/vndb.erl
+++ src/vndb.erl
@@ -2,7 +2,6 @@
 -export([connect/0, connect/3, cmd/2, close/1]).
 -export([login/2, dbstats/1, get/4, get/5]).
 -export([get_all/4, get_all/5]).
--export([nyaa/1]).
 
 % TODO: timeouts? send/2 and recv/2 have them set to infinity
 
