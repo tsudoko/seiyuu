c5e42e5fbd4af0c93b0d53da3ef40858f62a5262
flan <flan@flande.re>
2018-06-26 22:16:16 +0200

    Add UTF-8 aware URI decoding function

--- src/seiyuu_util.erl
+++ src/seiyuu_util.erl
@@ -1,6 +1,7 @@
 -module(seiyuu_util).
--export([bool/1, ht/1]).
+-export([bool/1, ht/1, uri_decode/1]).
 
 bool(0) -> false;
 bool(N) when N+1 == N+1 -> true.
 ht(Text) -> re:replace(re:replace(re:replace(Text, "<", "&lt;"), ">", "&gt;"), "&", "&amp;").
+uri_decode(S) -> unicode:characters_to_list(list_to_binary(http_uri:decode(S))).
